35fec3ea2c64cd46a20ea25897231cdb3fb05062
Sandor Kunyik <postmaster@kane-il.us>
2024-04-11 14:31:36 -0500

    Re-structured the MVC
    This is going to change again, as I read through the text book.

--- src/model.py
+++
@@ -1,9 +0,0 @@
-# model.py
-
-class WorkforceModel:
-    def __init__(self):
-        pass
-
-    def predict_workforce(self, parameters):
-        # Implementation of the predictive model
-        pass
--- src/view.py
+++
@@ -1,9 +0,0 @@
-# view.py
-
-class UserInterface:
-    def __init__(self):
-        pass
-
-    def display_results(self, results):
-        # Display the results to the user
-        pass
